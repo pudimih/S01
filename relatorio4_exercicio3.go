06cd36c38a4049a5c74b5dd3f5b05fada38a5a22
Millene Toledo <127104237+pudimih@users.noreply.github.com>
2025-09-11 22:41:42 -0300

    Create relatorio4_exercicio3.go

---
+++ relatorio4_exercicio3.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func fibonacci(n int) {
+    a, b := 0, 1
+    for i := 0; i < n; i++ {
+        fmt.Print(a, " ")
+        a, b = b, a+b
+    }
+    fmt.Println()
+}
+
+func main() {
+    var qtd int
+    fmt.Println("entre com a quantidade:")
+    fmt.Scanln(&qtd)
+
+    fibonacci(qtd)
+}
